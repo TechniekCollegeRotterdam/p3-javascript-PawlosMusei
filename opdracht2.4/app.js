05ad032cc5b3011c393617f05ded15ccbcb61390
Pawlos <9020097@student.zadkine.nl>
2023-02-09 13:26:50 +0100

    update

---
+++ opdracht2.4/app.js
@@ -0,0 +1,20 @@
+
+//opdracht1
+function calc(num, num2){
+   let answer = num + num2;
+   console.log(answer);
+}
+calc(10, 9)
+//opdracht2
+function multiplyByFive(num){
+    let answer = num * 5;
+    console.log(answer);
+}
+multiplyByFive(5);
+
+//opdracht 3
+function calculateMinutes(num){
+    let answer = num / 60
+    console.log(answer);
+}
+calculateMinutes(3600);
