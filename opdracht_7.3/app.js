c627a42ad61fdd300d90532152adbe85f95ff1ec
Pawlos <9020097@student.zadkine.nl>
2023-03-27 11:33:38 +0200

    update

---
+++ opdracht_7.3/app.js
@@ -0,0 +1,14 @@
+// for (let i = 0; i <= 100; i++){
+//     console.log(i);
+// }
+
+for (let i = 100; i >= 0; i--){
+    console.log(i);
+}
+
+
+// let shop = ["meloen", "apppel", "citroen"];
+
+// for (let i = 0; i < shop.length; i++){
+//     console.log(shop[i]);
+// }
